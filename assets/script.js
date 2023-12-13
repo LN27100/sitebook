31982771366cbb90dd3e5a7e787e2995e5ab7b35
LN27100 <heloceinlove@hotmail.fr>
2023-12-13 11:22:09 +0100

    affichage livres dans cards

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,46 @@ function getProduct() {
         .then(response => response.json())
         .then(data => {
             updatePageWithData(data);
-            return data;  
+            return data;
         });
 }
 
 function updatePageWithData(data) {
     const resultElement = document.getElementById('products');
-    
+
     if (resultElement) {
-        resultElement.textContent = JSON.stringify(data, null, 2);
+
+        resultElement.innerHTML = '';
+
+
+        data.forEach(product => {
+
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.style = 'width: 18rem;';
+
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+
+            cardBody.innerHTML = `
+                <img src="../assets/Pictures/${product.Image}.jpg" class="mx-auto d-block" alt="Product Image"></img>
+                <h5 class="card-title">${product.Titre}</h5>
+                <p class="card-ref">${product.Référence}</p>
+                <p class="card-description">${product.description}</p>
+                <p class="card-autor">Auteur: ${product.Auteur}</p>
+                <p class="card-stock">Stock : ${product.Stock} unité(s)</p>
+                <p class="card-price">${product.Prix}€</p>
+                <a href="#" class="btn btn-primary">Panier</a>
+            `;
+
+
+            card.appendChild(cardBody);
+
+
+            resultElement.appendChild(card);
+        });
     } else {
         console.error('Élément non trouvé');
     }
