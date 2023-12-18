cbca97f9cb389899cc3f66d6512a18a361991605
LN27100 <heloceinlove@hotmail.fr>
2023-12-18 15:16:37 +0100

    mise à jour chemins

--- app/assets/script.js
+++ app/assets/script.js
@@ -368,30 +368,10 @@ function removeProduct(titre) {
     updateCartDisplay();
 }
 
-// Formulaire connaxion
+// FORMULAIRE DE CONNEXION
 
-let cgu = document.getElementById("cgu");
-let majeur = document.getElementById("majeur");
 let button = document.getElementById("button");
 
-function checkedd() {
-    console.log('ok')
-    if (cgu.checked && majeur.checked) {
-        button.disabled = false;
-    } else {
-        button.disabled = true;
-    }
-
-}
-
-cgu.addEventListener("change", checkedd);
-
-majeur.addEventListener("change", checkedd);
-
-
-document.getElementById("button").addEventListener("click", function (event) {
-    document.cookie = "cgu=on"
-})
 
 function getCookie(name) {
     const cookies = document.cookie.split('; ')
@@ -403,9 +383,4 @@ function getCookie(name) {
     }
 } 
 
-if (getCookie('cgu')) {
-    window.location.href = '../HTML/search.html';
-    exit()
-}
 
-console.log(getCookie("toto"))
